4d7f48df2458672fdccebaeda5700425671569d5
AnjuBala <anjubudhwan@gmail.com>
2019-10-25 13:09:55 +0200

    redux and redux thunk added

---
+++ my-app/src/actions/index.ts
@@ -0,0 +1,5 @@
+import { Action, Dispatch } from 'redux';
+
+export const ACTION_NOTES_FETCH = 'NOTES_FETCH';
+export const ACTION_NOTES_FETCH_SUCCESS = 'NOTES_FETCH_SUCCESS';
+export const ACTION_NOTES_FETCH_ERROR = 'NOTES_FETCH_ERROR';
