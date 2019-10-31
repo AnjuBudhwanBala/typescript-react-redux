3f1a6831ff41939ae889b418ed3fe1b2e6072d26
AnjuBala <anjubudhwan@gmail.com>
2019-10-31 16:34:41 +0100

    reducers and action creators are added

--- login/src/App.tsx
+++ login/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Login } from './Login';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Login></Login>>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Login></Login>>
+      </div>
+    </Provider>
   );
 };
 
